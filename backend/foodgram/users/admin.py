a820dda204391f5303feaf8e30af8e66909cd6fa
Aleksey Kazikov <kas.alex29@gmail.com>
2023-09-13 21:34:06 +0300

    Диплом: 1 часть: 1

---
+++ backend/foodgram/users/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import User, Follow
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name')
+    list_filter = ('email', 'first_name')
+
+
+@admin.register(Follow)
+class SubscribeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author')
